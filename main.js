12840b6ffb3d75f7c557eb445aefb65456a268d6
jose.adelson2002 <jose.adelson2002@academico.ufs.br>
2025-04-23 15:27:08 -0300

    atribui simbolos, adiciona mudança de turno e verifica vencedor

--- main.js
+++ main.js
@@ -1,12 +1,84 @@
 const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
+const winningMessageElement = document.querySelector("[data-winning-message]");
 
-for (const cell of cellElements) {
-  cell.addEventListener("click", handleClick, { onde: true });
+let isCircleTurn;
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const startGame = () => {
+  for (const cell of cellElements) {
+    cell.addEventListener("click", handleClick, { once: true });
+  }
+
+  isCircleTurn = false;
+  board.classList.add("x");
+};
+
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winningMessageTextElement.innerText = "Empate!";
+  } else {
+    winningMessageTextElement.innerText = isCircleTurn
+      ? "O Venceu!"
+      : "X Venceu!";
+  }
+  winningMessageElement.classList.add("show-winning-message");
+};
+
+const checkForWin = (currentPlayer) => {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentPlayer);
+    });
+  });
+};
+
+const placeMark = (cell, classToAdd) => {
+  cell.classList.add(classToAdd);
+};
+
+const swapTurns = () => {
+  isCircleTurn = !isCircleTurn;
+
+  board.classList.remove("circle");
+  board.classList.remove("x");
+
+  if (isCircleTurn) {
+    board.classList.add("circle");
+  } else {
+    board.classList.add("x");
+  }
+};
+
+function handleClick(e) {
+  // Colocar o símbolo (X ou Círculo)
+  const cell = e.target;
+  const classToAdd = isCircleTurn ? "circle" : "x";
+  placeMark(cell, classToAdd);
+
+  // Verificar por vitória
+  const isWin = checkForWin(classToAdd);
+  if (isWin) {
+    endGame(false);
+  }
+  // Verificar por empate
+
+  // Trocar de símbolo
+
+  swapTurns();
 }
 
-const handleClick = () => {
-  // Colocar o símbolo (X ou Círculo)
-  // Verificar por vitória
-  // Verificar por empate
-  // Trocar de símbolo
-};
+startGame();
